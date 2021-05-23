677b36e396f91ae3b1480a56ca8601a1b0a3271a
YuriiVavrynchuk <yuriyu.com@ukr.net>
2021-05-23 13:30:18 +0300

    initial commit

---
+++ src/main/java/org/example/medic/repository/SurgeonRepository.java
@@ -0,0 +1,9 @@
+package org.example.medic.repository;
+
+import org.example.medic.models.Surgeon;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SurgeonRepository extends CrudRepository<Surgeon, Long> {
+}
